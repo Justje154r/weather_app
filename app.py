7d0d2acc886f7560131324a0e764731e96d16e05
Mikhail_Belov <mikhail.belov@tul.cz>
2024-05-22 22:04:18 +0100

    3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import requests
 from config import API_KEY
 
@@ -17,9 +17,22 @@ def get_weather(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        weather = response.json()
+        weather_icon = get_weather_icon(weather['weather'][0]['main'])
+        weather['icon_url'] = url_for('static', filename=f'img/{weather_icon}')
+        return weather
     else:
         return None
+
+def get_weather_icon(weather_condition):
+    icons = {
+        'Clear': 'sunny.png',
+        'Clouds': 'cloudy.png',
+        'Rain': 'rainy.png',
+        'Snow': 'snowy.png',
+        'Thunderstorm': 'stormy.png'
+    }
+    return icons.get(weather_condition, 'unknown.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
